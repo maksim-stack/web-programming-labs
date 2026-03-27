c1a51fbcf89d9dfe9a1197f451615ffbea411cd3
Maksim Borminskyi <mborminskij@gmail.com>
2026-03-27 19:33:10 +0200

    Lab 5: Add todo list with useQuery

--- web-programming-labs/lab5/src/App.tsx
+++ web-programming-labs/lab5/src/App.tsx
@@ -1,5 +1,37 @@
+import { useQuery } from '@tanstack/react-query'
+import { todosApi } from './api/todos'
+
 function App() {
-  return <div>Lab 5</div>
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ['todos'],
+    queryFn: todosApi.getAll
+  })
+
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
+
+  if (isError) {
+    return <div>Error loading todos</div>
+  }
+
+  return (
+    <div>
+      <h1>Todos</h1>
+      <ul>
+        {data?.map((todo) => (
+          <li 
+            key={todo.id} 
+            style={{
+              textDecoration: todo.completed ? 'line-through' : 'none',
+            }}
+          >
+          {todo.title}
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
 }
 
 export default App
